6bd526583089d71775f48b6f4005ffb81bb0753a
qc_993a789498204364b02a3c9de2514d84 <qc_993a789498204364b02a3c9de2514d84@git.code.tencent.com>
2019-07-19 16:42:21 +0800

    添加提案附议页面样式描述.1

--- src/views/proposal/support.vue
+++ src/views/proposal/support.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="app-container">
     <aside>
-      supportProposal 附议提案
+      supportProposal 附议提案---------
+      左侧显示自己已提交的提案列表和每个提案对应的附议人数，提供按钮“邀请”---------
+      右侧显示他人已提交的提案列表和每个提案对应的附议人数，提供按钮“附议”---------
+      发送方选择接收方并点击发送邀请之后，接收方会在Message中收到一条新消息，消息内容包含附议的链接
     </aside>
   </div>
 </template>
