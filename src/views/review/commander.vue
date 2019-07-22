593bb349c7f6f746f830a1e4cd12b8911fb7f5c4
qc_993a789498204364b02a3c9de2514d84 <qc_993a789498204364b02a3c9de2514d84@git.code.tencent.com>
2019-07-22 11:16:15 +0800

    添加代表团团长审核页面要求描述.1“

--- src/views/review/commander.vue
+++ src/views/review/commander.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="app-container">
     <aside>
-      Commander Review 代表团团长审核
+      代表团团长审核-------------显示附议人数不小于两人的提案列表，每个提案提供按钮包括“详情”、“通过”、“驳回”；------------
+      点击“详情”可以查看该提案具体信息；------------
+      点击“通过”则该提案会在提案组审核页面显示,同时更新该提案的状态；------------
+      点击“驳回”则该提案的状态更改为“草稿”，仅该提案提出者在自己的草稿箱中可见
     </aside>
     <el-button icon="el-icon-question" type="primary" @click.prevent.stop="guide">
       Show Guide
