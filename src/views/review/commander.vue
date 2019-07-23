7d482fcbc2b5eb6a2b5b5479811d92ff07eb8584
qc_993a789498204364b02a3c9de2514d84 <qc_993a789498204364b02a3c9de2514d84@git.code.tencent.com>
2019-07-23 11:48:43 +0800

    add:新增代表团团长审核页面样式

--- src/views/review/commander.vue
+++ src/views/review/commander.vue
@@ -6,31 +6,126 @@
       点击“通过”则该提案会在提案组审核页面显示,同时更新该提案的状态；------------
       点击“驳回”则该提案的状态更改为“草稿”，仅该提案提出者在自己的草稿箱中可见
     </aside>
-    <el-button icon="el-icon-question" type="primary" @click.prevent.stop="guide">
-      Show Guide
-    </el-button>
+    <el-table
+    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+    style="width: 98%">
+    <el-table-column
+      label="日期"
+      width="180">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="提案名"
+      width="360">
+      <template slot-scope="scope">
+        <el-popover trigger="hover" placement="top">
+          <p>姓名: {{ scope.row.name }}</p>
+          <p>附议人数: {{ scope.row.proponum }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-tag type="primary">{{ scope.row.proponame }}</el-tag>
+          </div>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column
+      align="right">
+      <template slot="header" slot-scope="scope">
+        <el-input
+          v-model="search"
+          size="mini"
+          placeholder="输入关键字搜索"/>
+      </template>
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          type="primary"
+          @click="handleEdit(scope.$index, scope.row)">详情</el-button>
+        <el-button
+          size="mini"
+          type="success"
+          @click="handlePass(scope.$index, scope.row)">通过</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">驳回</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
   </div>
 </template>
 
 <script>
 import Driver from 'driver.js' // import driver.js
 import 'driver.js/dist/driver.min.css' // import driver.js css
-import steps from './steps'
+
 
 export default {
   name: 'CommanderReview',
   data() {
     return {
-      driver: null
+      tableData: [{
+        propoId: '009',
+        date: '2016-05-02',
+        name: '冯伟横',
+        proponame: '关于奉贤校区新建食堂建议',
+        proponum: '7',
+        checked: false
+      }, {
+        propoId: '008',
+        date: '2016-05-04',
+        name: '王小虎',
+        proponame: '关于徐汇校区修建宿舍楼问题',
+        proponum: '6',
+        checked: true
+      }, {
+        propoId: '007',
+        date: '2016-05-01',
+        name: '冯伟狠',
+        proponame: '垃圾分类建议',
+        proponum: '4',
+        checked: false
+      }, {
+        propoId: '006',
+        date: '2016-05-03',
+        name: '王小虎',
+        proponame: '教工调整',
+        proponum: '2',
+        checked: false
+      },
+      {
+        propoId: '005',
+        date: '2016-05-01',
+        name: '冯伟很',
+        proponame: '关于奉贤校区新建游泳馆的建议',
+        proponum: '4',
+        checked: true
+      },
+      {
+        propoId: '004',
+        date: '2016-05-02',
+        name: '王小虎',
+        proponame: '关于奉贤食堂会议',
+        proponum: '7',
+        checked: false
+      }],
+        search: ''
     }
   },
   mounted() {
     this.driver = new Driver()
   },
   methods: {
-    guide() {
-      this.driver.defineSteps(steps)
-      this.driver.start()
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handlePass(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
     }
   }
 }
