52d003491deebc76fcea3e8c19a05358d3206f22
FXLP <1399774043@qq.com>
2019-07-30 11:19:37 +0800

    add审阅页面样式

---
+++ src/views/verify/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="app-container">
+    <el-button size="mini" type="primary">校领导审阅</el-button>
+    <el-table :data="list" style="width: 98%">
+      <el-table-column
+        label="日期"
+        width="180"
+      >
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="提案名"
+        width="360"
+      >
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>提案发起者: {{ scope.row.name }}</p>
+            <p>附议人数: {{ scope.row.proponum }}</p>
+            <p>提案状态: {{ scope.row.propostate }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag type="primary">{{ scope.row.proponame }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column
+        align="right"
+      >
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            @click="goToDetail(scope.$index, scope.row)"
+          >详情</el-button>
+          <el-button
+            size="mini"
+            type="success"
+            @click="handleModifyStatus(scope.row,'已审阅')"
+          >已审阅</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+  </div>
+</template>
+
+<script>
+import { fetchWaitHandleList } from '@/api/proposal'
+import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+
+export default {
+  name: 'CommanderReview',
+  components: { Pagination },
+  data() {
+    return {
+      list: null,
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 10
+      },
+      search: ''
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      fetchWaitHandleList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+      })
+    },
+    goToDetail(index, row) {
+      const p = '/proposal/propodetail/' + this.list[index].propoId
+      this.$router.push({ path: p })
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: '操作成功',
+        type: 'success'
+      })
+      row.propostate = status
+      console.log(row.propostate)
+    }
+  }
+}
+</script>
